ab3df467adcaa269e2982f3e45019c36b5b807f7
jiwonmik <jiwonmik@gmail.com>
2023-02-08 22:43:05 +0900

    edit Routing based on Login Info

--- src/Router.js
+++ src/Router.js
@@ -1,28 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import PrivateRoute from "./Routes/PrivateRoute";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Todo from "./pages/Todo";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import { useUserState } from "./UserContext";
 
 function Router(){
-    const access = localStorage.getItem('access_token');
+    const authState = useUserState();
 
-    // const [userLogged, setUserLogged] = useState(JSON.parse(localStorage.getItem("access_token")));
-        
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/signin" element={<SignIn/>}/>
-            <Route path="/todo" 
-                element={
-                    <PrivateRoute
-                        authenticated={access}
-                        component={<Todo/>}
-                />}
-            />
+            <Route path="/signup" element={
+                !(authState.token) ? 
+                <SignUp/> : <Navigate to="/todo"/>}/>
+            <Route path="/signin" element={!(authState.token) ? 
+                <SignIn/> : <Navigate to="/todo"/>}/>
+            <Route path="/todo" element={
+                !(authState.token) ? 
+                (<Navigate to="/signin"/>) : <Todo/>
+            }/>
         </Routes>
     </BrowserRouter>
 }
